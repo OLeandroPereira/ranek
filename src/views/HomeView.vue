27596a4deb65f78e55d1ed7b7cbc34b5e3952ba1
Leandro Marques <leandroppmarques@hotmail.com>
2023-04-09 23:50:04 -0300

    commit inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
   </div>
 </template>
 
@@ -11,8 +9,5 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
