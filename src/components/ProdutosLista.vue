d41e170dfefdcfa7de3dbdfa981f78e1d323e528
Leandro Marques <leandroppmarques@hotmail.com>
2023-04-16 23:56:53 -0300

    estilizando componente Produto Lista

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,10 +1,17 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p>{{ produto.descricao }}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length == 0">
+      <p>Busca sem resultados. Tente buscar por outro.</p>
     </div>
   </section>
 </template>
@@ -43,3 +50,24 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+  .produtos-container {
+    max-width: 1000px;
+    margin: 0 auto;    
+  }
+
+  .produto {
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+    padding: 10px;
+    background: #fff;
+    border-radius: 4px;
+    transition: all 0.2s;
+  }
+  .produtos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin: 30px;
+  }
+</style>
