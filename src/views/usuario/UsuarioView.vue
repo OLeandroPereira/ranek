390204e05cdb7e389067225fcfaa5939b7482a9f
Leandro Marques <leandroppmarques@hotmail.com>
2023-04-30 23:55:59 -0300

    link usuário

--- src/views/usuario/UsuarioView.vue
+++ src/views/usuario/UsuarioView.vue
@@ -1,5 +1,29 @@
 <template>
-  <p>Página do Usuário: {{ $store.state.usuario }}</p>
+  <section class="usuario">
+    <nav class="sidenav">
+      <ul>
+        <li>
+          <router-link to="/">Produtos</router-link>
+        </li>
+        <li>
+          <router-link to="/">Compras</router-link>
+        </li>
+        <li>
+          <router-link to="/">Vendas</router-link>
+        </li>
+        <li>
+          <router-link to="/">Editar Usuário</router-link>
+        </li>
+        <li>
+          <router-link to="/">Deslogar</router-link>
+        </li>
+      </ul>        
+    </nav>
+    <transition mode="out-in">
+      <router-view></router-view>
+    </transition>
+    <p>Página do Usuário: {{ $store.state.usuario }}</p>
+  </section>
 </template>
 
 <script>
