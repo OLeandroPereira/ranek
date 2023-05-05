683f518cc747524abfb680c2f46fb0eb7292cee0
Leandro Marques <leandroppmarques@hotmail.com>
2023-05-04 22:52:38 -0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,52 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ProdutoView from '../views/ProdutoView.vue'
-import LoginView from '../views/LoginView.vue'
-import UsuarioView from '../views/usuario/UsuarioView.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import ProdutoView from '../views/ProdutoView.vue';
+import LoginView from '../views/LoginView.vue';
+import UsuarioView from '../views/usuario/UsuarioView.vue';
+import UsuarioProdutos from '../views/usuario/UsuarioProdutos.vue';
 
-Vue.use(VueRouter)
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/produto/id:',
-    name: 'produto',
-    component: ProdutoView,
-    props: true
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView,
-  },
-  {
-    path: '/usuario',
-    name: 'usuario',
-    component: UsuarioView,
-  }
-]
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: HomeView,
+      },
+      {
+        path: '/produto/id:',
+        name: 'produto',
+        component: ProdutoView,
+        props: true
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: LoginView,
+      },
+      {
+        path: '/usuario',
+        component: UsuarioView,
+        children: [
+          {
+            path: "",
+            name: "usuario",
+            component: UsuarioProdutos,
+          }
+        ]
+      }
+    ],
+    scrollBehavior() {
+      return window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  });
+  
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-  scrollBehavior() {
-    return window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-})
 
-export default router
+
+
+
