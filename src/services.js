e2dc3fc6e081e1d7ef64d6b6ab66a9f20370cd40
Leandro Marques <leandroppmarques@hotmail.com>
2023-05-17 23:54:32 -0300

    service delete

--- src/services.js
+++ src/services.js
@@ -11,6 +11,9 @@ export const api = {
   },
   post(endpoint, body) {
     return axiosInstance.get(endpoint, body);
+  },
+  delete(endpoint) {
+    return axiosInstance.delete(endpoint);
   }
 };
 
