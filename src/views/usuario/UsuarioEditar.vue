3710086912d0b1405b0b4583bcf041ac7d72dda6
Leandro Marques <leandroppmarques@hotmail.com>
2023-05-19 23:56:15 -0300

    Usuario Editar

---
+++ src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuáario</button>
+    </UsuarioForm>
+  </section>    
+</template>
+
+<script>
+import UsuarioForm from "@/components/UsuarioForm.vue";
+
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm
+  }
+};
+</script>
+
+<style>
+</style>
